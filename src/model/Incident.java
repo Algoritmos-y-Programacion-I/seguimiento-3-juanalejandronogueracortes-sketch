14027c03257ddc428c32d5cb97b165f54b77c9d1
Juan Alejandro Noguera Cortes <1110293979@u.icesi.edu.co>
2025-10-24 13:00:20 -0500

    first

--- src/model/Incident.java
+++ src/model/Incident.java
@@ -1,5 +1,47 @@
 package model;
+import java.time.LocalDate;
 
-public class Incident {
-    
+public class Incident { 
+
+    private LocalDate dateReport; 
+    private String description; 
+    private boolean solution; 
+    private int solutionHours;
+
+    public incident (LocalDate dateReport, String description) {
+        this.dateReport = dateReport; 
+        this.description = description; 
+    }
+
+    public LocalDate getDateReport() {
+        return dateReport;
+    }
+
+    public String getDescription () {
+        return description;
+    }
+
+    public boolean getSolution () {
+        return solution;
+    }
+
+    public int getSolutionHours () {
+        return solutionHours;
+    }
+
+    public void setDateReport(LocalDate dateReport) {
+        this.DateReport = dateReport; 
+    }
+
+    public void setDescription(String description) {
+        this.Description = description; 
+    }
+
+    public void setSolution(boolean solution){
+        this.Solution = solution;
+    }
+
+    public void setSolutionHours(int solutionHours) {
+        this.SolutionHours = solutionHours;
+    }
 }
